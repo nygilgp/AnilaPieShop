cb5c7e3225ffb014f8ed951d17ab84700f16593b
Nygil Pereira <nygilgp@gmail.com>
2019-04-12 02:02:02 +0530

    Feat: Added model folder and pie class, iterator class and mock class Added service of model to startup

---
+++ AnilaPieShop/Models/IPieRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AnilaPieShop.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> GetAllPies();
+        Pie GetPieById(int pieId);
+    }
+}
---
+++ AnilaPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnilaPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private List<Pie> _pies;
+
+        public MockPieRepository()
+        {
+            if(_pies == null)
+            {
+                initializePie();
+            }
+        }
+
+        public void initializePie()
+        {
+            _pies = new List<Pie>
+            {
+                new Pie {Id = 1, Name = "Apple Pie", ShortDescription = "Short", Price = 10.9M },
+                new Pie {Id = 2, Name = "Blue Berry Pie", ShortDescription = "Short", Price = 12.9M },
+                new Pie {Id = 3, Name = "Strawberry Pie", ShortDescription = "Short", Price = 11.9M },
+                new Pie {Id = 4, Name = "Banana Pie", ShortDescription = "Short", Price = 15.9M },
+
+            };
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _pies.FirstOrDefault(p => p.Id == pieId);
+        }
+    }
+}
