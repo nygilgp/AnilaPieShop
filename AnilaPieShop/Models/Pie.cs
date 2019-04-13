f462f20deb3c74c7ec9a69d97ff691f3eaa8877a
Nygil Pereira <nygilgp@gmail.com>
2019-04-13 23:32:36 +0530

    Fix: Changed field name

--- AnilaPieShop/Models/Pie.cs
+++ AnilaPieShop/Models/Pie.cs
@@ -3,6 +3,15 @@ namespace AnilaPieShop.Models
 {
     public class Pie
     {
+        public int Id { get; set;  }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public bool IsPieOfTheWeek { get; set; }
+        public string ImageUrl { get; internal set; }
+        public string ImageThumbnailUrl { get; internal set; }
+
         public Pie()
         {
         }
