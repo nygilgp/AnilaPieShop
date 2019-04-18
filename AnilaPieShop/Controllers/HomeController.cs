c0606934614f0604406d6907e4f79c23e52d400a
Nygil Pereira <nygilgp@gmail.com>
2019-04-19 03:36:26 +0530

    Feat: Added details view Added Routes in startup file Added View imports In Layout added navigation

--- AnilaPieShop/Controllers/HomeController.cs
+++ AnilaPieShop/Controllers/HomeController.cs
@@ -32,5 +32,15 @@ namespace AnilaPieShop.Controllers
 
             return View(homeViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if(pie == null)
+            {
+                return NotFound();
+            }
+            return View(pie);
+        }
     }
 }
--- AnilaPieShop/Startup.cs
+++ AnilaPieShop/Startup.cs
@@ -6,18 +6,31 @@ using AnilaPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AnilaPieShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get;  }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options 
+                => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             // Add Transient means, when ever we reques for IPieRepository, will return MockPieRepository
-            services.AddTransient<IPieRepository, MockPieRepository>();
+            //services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddTransient<IPieRepository, PieRepository>();
 
             services.AddMvc();
         }
@@ -38,7 +51,14 @@ namespace AnilaPieShop
             app.UseStatusCodePages();
             app.UseStaticFiles();
 
-            app.UseMvcWithDefaultRoute();
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Home}/{action=Index}/{id?}"
+                    ); 
+            });
         }
     }
 }
