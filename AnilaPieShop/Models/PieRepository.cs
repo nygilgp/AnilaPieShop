36dd326454cd963983759c862da1fa613eed1ba5
Nygil Pereira <nygilgp@gmail.com>
2019-04-19 03:37:31 +0530

    Added get all pies and get pie by id

--- AnilaPieShop/Models/PieRepository.cs
+++ AnilaPieShop/Models/PieRepository.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AnilaPieShop.Models
 {
-    public class PieRepository
+    public class PieRepository : IPieRepository
     {
-        public PieRepository()
+        private readonly AppDbContext _appDbContext;
+
+        public PieRepository(AppDbContext appDbContext)
         {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _appDbContext.Pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _appDbContext.Pies.FirstOrDefault(p => p.Id == pieId);
         }
     }
 }
